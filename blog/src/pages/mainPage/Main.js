1174f175915109317b448c04bbe3d5f805e5e721
JunOnJuly <wlgnstjd0717@gmail.com>
2023-09-11 14:12:11 +0900

    init blog

---
+++ blog/src/pages/mainPage/Main.js
@@ -0,0 +1,12 @@
+import * as s from './Main_css'
+
+function Main() {
+    return (
+      <s.MainBackgroundDiv className="App">
+        <p>hi</p>
+      </s.MainBackgroundDiv>
+    );
+  }
+  
+  export default Main;
+  
---
+++ blog/src/pages/mainPage/Main_css.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import * as c from '../../design/Colors'
+
+export const MainBackgroundDiv = styled.div`
+    position: absolute;
+    right: 0;
+    top: 0;
+
+    display: flex;
+    flex-direction: column;
+
+    height: 100vh;
+    width: 80vw;
+
+    background-color: ${c.Colors.main_blue};
+`
